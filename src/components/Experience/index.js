91e1770c57c8ec2f28f302c100dc9b66f387541d
oluapcamargo@gmail.com <paulo.camargo@edenred.com>
2024-06-03 10:04:33 -0300

    Add project

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -93,7 +93,6 @@ const index = () => {
                             </TimelineItem>
                         ))}
                     </Timeline>
-
                 </TimelineSection>
             </Wrapper>
         </Container>
