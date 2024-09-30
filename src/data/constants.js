eafd9ec1d105b8bf360bec758ca7473b0b17686b
Paulo Henrique Camargo <oluapcamargo@gmail.com>
2024-09-30 11:35:47 -0300

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -14,7 +14,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/oluapcamargo",
     resume:
-      "https://drive.google.com/file/d/1dRJSPliaqe7fH-LRghvtnhuGHKLUmcsb/view?usp=drive_link",
+      "https://drive.google.com/file/d/1Q7JWxLX4qWhPkAr3g2f3WCUrM-xkamaa/view?usp=drive_link",
     linkedin: "https://www.linkedin.com/in/paulo-henrique-camargo-95621526/"
   };
   
